d109cb9d6c74ac3e0ccf9aea240d899963de5b94
Frank An <anfrank2015@gmail.com>
2026-06-29 19:01:08 -0700

    Add: surrounded-regions - submission-5

---
+++ Data Structures & Algorithms/surrounded-regions/submission-5.py
@@ -0,0 +1,33 @@
+class Solution:
+    def solve(self, board: List[List[str]]) -> None:
+        
+        visited = set()
+        directions = [[0, 1], [0, -1], [1, 0], [-1, 0]]
+        paths = []
+        def dfs(i, j):
+            stack = [(i, j)]
+            path = []
+            while stack:
+                n = stack.pop()
+                if n not in visited:
+                    visited.add(n)
+                    n_i, n_j = n
+                    if n_i == 0 or n_j == 0 or n_i == len(board)-1 or n_j == len(board[0])-1:
+                        if board[n_i][n_j] == "X":
+                            paths.append(path)
+                        return 
+                    if board[n_i][n_j] == "X":
+                        continue
+                    for x,y in directions:
+                        stack.append((n_i+x, n_j+y))
+                    path.append((n_i, n_j))
+
+        for i in range(1, len(board)-1):
+            for j in range(1, len(board[0])-1):
+                if board[i][j] == "O":
+                    dfs(i, j)
+
+        print(paths)
+        for path in paths:
+            for i, j in path:
+                board[i][j] = "X"
