3992fdadff76a5d1e6823a7afe4235734bfefcc0
Frank An <anfrank2015@gmail.com>
2026-07-16 18:17:39 -0700

    Add: house-robber-ii - submission-1

---
+++ Data Structures & Algorithms/house-robber-ii/submission-1.py
@@ -0,0 +1,12 @@
+class Solution:
+    def rob(self, nums: List[int]) -> int:
+        if len(nums) < 4:
+            return max(nums)
+        one, two = 0, 0
+        nums.append(nums[0])
+        for n in nums[1:len(nums)]:
+            temp = max(n+one, two)
+            one = two
+            two = temp
+            
+        return two
