b11c48bd3308b12197f5a397e129ff49d14b257b
Frank An <anfrank2015@gmail.com>
2026-07-20 00:18:23 -0700

    Add: merge-intervals - submission-1

---
+++ Data Structures & Algorithms/merge-intervals/submission-1.py
@@ -0,0 +1,12 @@
+class Solution:
+    def merge(self, intervals: List[List[int]]) -> List[List[int]]:
+        intervals.sort(key = lambda x:x[0])
+        merged = [intervals[0]]
+        for current in intervals[1:]:
+            previous = merged[-1]
+            if current[0] <= previous[1]:
+                merged[-1][1] = current[1]
+            else:
+                merged.append(current)
+
+        return merged
