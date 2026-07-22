a2907fd4d8bf25b6275bd3485b284bb677ddc03d
Frank An <anfrank2015@gmail.com>
2026-07-22 00:23:18 -0700

    Add: meeting-schedule-ii - submission-3

---
+++ Data Structures & Algorithms/meeting-schedule-ii/submission-3.py
@@ -0,0 +1,21 @@
+"""
+Definition of Interval:
+class Interval(object):
+    def __init__(self, start, end):
+        self.start = start
+        self.end = end
+"""
+
+class Solution:
+    def minMeetingRooms(self, intervals: List[Interval]) -> int:
+        intervals.sort(key = lambda x:x.start)
+        count = [[intervals[0]]]
+        previous = intervals[0].end
+        for i in intervals[1:]:
+            if i.start > previous:
+                count[len(count)-1].append(i)
+            else:
+                count.append([i])
+            previous = i.end
+        return len(count)
+
