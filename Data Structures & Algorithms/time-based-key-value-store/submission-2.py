4c651b792ac7c4b41ca16377fda852b7843bef88
Frank An <anfrank2015@gmail.com>
2026-08-16 07:23:04 +0800

    Add: time-based-key-value-store - submission-2

---
+++ Data Structures & Algorithms/time-based-key-value-store/submission-2.py
@@ -0,0 +1,33 @@
+class TimeMap:
+
+    def __init__(self):
+        self.time = {}
+        #key: [value, timestamp]
+
+    def set(self, key: str, value: str, timestamp: int) -> None:
+        if key not in self.time:
+            self.time[key] = []
+        self.time[key].append([value, timestamp])
+
+    def get(self, key: str, timestamp: int) -> str:
+        if key not in self.time:
+            return ""
+        if timestamp > self.time[key][-1][1]:
+            return self.time[key][len(self.time[key])-1][0]
+        #binary search
+        left = 0
+        right = len(self.time[key])-1
+        r = ""
+        while left <= right:
+            mid = (left + right) // 2
+
+            t = self.time[key][mid][1]
+            if t == timestamp:
+                return self.time[key][mid][0]
+            if t > timestamp:
+                right = mid - 1
+            else:
+                r = self.time[key][mid][0]
+                left = mid + 1
+        return r
+
